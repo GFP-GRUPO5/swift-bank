0d5b62c2c80897260ce5b373a427ec5de548f5a7
Bruno Russo <bruno@jumpstartmd.com>
2025-03-27 09:05:10 -0300

    feat: add SearchInput component

---
+++ components/molecules/search-input/SearchInput.styles.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const searchInputStyles = StyleSheet.create({
+  wrapper: {
+    position: 'relative',
+  },
+  input: {},
+  iconWrapper: {
+    position: 'absolute',
+    right: 8,
+  },
+})
