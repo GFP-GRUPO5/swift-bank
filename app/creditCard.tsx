0b918490f4964aeb5c8dc55712dd5713cb7eaa05
Rodrigo Harder <rodrigo.harder@alura.com.br>
2025-03-28 19:51:16 -0300

    feat: add pages

---
+++ app/creditCard.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function CreditCard(){
+    return (
+        <View>
+            <Text>Peça seu cartão de crédito agora mesmo!</Text>
+        </View>
+    )
+}
