a5e6f321ae00ececc4a144004b945d6910cd7443
Bruno Russo <bdmrusso@gmail.com>
2025-03-31 21:41:32 -0300

    feat: add auth reducer

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,16 @@
+import { updateAccessToken } from "@/redux/features/auth/authSlice";
+import { useAppDispatch } from "@/redux/hooks";
 import StoreProvider from "@/redux/StoreProvider";
 import { Slot } from "expo-router";
+import { useEffect } from "react";
 
 export default function RootLayout() {
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    dispatch(updateAccessToken({ accessToken: 'goiaba' }))
+  }, [])
+
   return (
     <StoreProvider>
       <Slot />
--- redux/store.ts
+++ redux/store.ts
@@ -2,13 +2,15 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { FLUSH, PAUSE, PERSIST, persistReducer, PURGE, REGISTER, REHYDRATE } from 'redux-persist';
 
+import authReducer from './features/auth/authSlice';
+
 const persistConfig = {
   key: 'root',
   storage: AsyncStorage
 }
 
 const rootReducer = combineReducers({
-
+  auth: authReducer,
 })
 
 const persistedReducer = persistReducer(persistConfig, rootReducer)
