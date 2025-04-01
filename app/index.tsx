b68d3ffc3163474016ed8bdb7ae84d4c5b767f04
Bruno Russo <bdmrusso@gmail.com>
2025-03-31 21:07:31 -0300

    Reapply "[SB-6] - Configuração do Firebase"
    This reverts commit cd530096c42acfec68a918020f985d65e15f65e2.

--- app/index.tsx
+++ app/index.tsx
@@ -1,26 +1,13 @@
-import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
-import { Text, View } from "react-native";
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { Button } from '../components/atoms/button/Button';
-import { TextInput } from '../components/atoms/text-input/TextInput';
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { Extract } from "@/components/molecules/extract/Extract";
+import { BackgroundGradient } from "@/components/templates/background-gradient/BackgroundGradient";
 
 export default function Index() {
   return (
     <GestureHandlerRootView>
-      <View
-        style={{
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Text>Edit app/index.tsx to edit this screen.</Text>
-        <Button label="Botão com label" />
-        <Button>
-          <FontAwesome6 name="magnifier" size={24} color="black" />
-        </Button>
-        <TextInput />
-      </View>
+      <BackgroundGradient>
+        <Extract />
+      </BackgroundGradient>
     </GestureHandlerRootView>
   );
 }
---
+++ app/investment.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Investment(){
+    return (
+        <View>
+            <Text>Invista seu dinheiro!</Text>
+        </View>
+    )
+}
