1bc53f98cb84f051e9075d6f4ce486fd40d73e4e
Rodrigo Harder <rodrigo.harder@alura.com.br>
2025-04-04 16:27:22 -0300

    fix: change name of component Button for ButtonAction

---
+++ components/atoms/button-action/ButtonAction.styles.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+export const buttonActionStyle = StyleSheet.create({
+  wrapper:{
+    backgroundColor:"#2C2C2C",
+    width:"100%",
+    borderRadius:24,
+    paddingVertical:12
+  },
+  text:{
+    fontSize:16,
+    fontWeight:"bold",
+    color:"#EDEDED",
+    textAlign:"center"
+  }
+})
