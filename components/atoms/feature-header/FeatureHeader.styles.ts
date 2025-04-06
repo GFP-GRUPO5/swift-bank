76ec656535f602d09ee247ded930978588d1a38e
Rodrigo Harder <rodrigo.harder@alura.com.br>
2025-04-06 13:10:47 -0300

    feat: add FeatureHeader

---
+++ components/atoms/feature-header/FeatureHeader.styles.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+export const featureHeaderStyles = StyleSheet.create({
+  header: {
+    gap: 16,
+    marginBottom: 24,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#2C2C2C",
+  }
+})
