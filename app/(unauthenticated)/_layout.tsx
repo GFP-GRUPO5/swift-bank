e46487cfa6225710a1ce9be75085f13516281698
Bruno Tavares <bdmrusso@gmail.com>
2025-04-10 23:35:26 -0300

    feat: remove header from unauthenticated screens

--- app/(unauthenticated)/_layout.tsx
+++ app/(unauthenticated)/_layout.tsx
@@ -1,8 +1,9 @@
 import { Stack } from "expo-router";
+import { Text } from "react-native";
 
 export default function SignUpLayout() {
   return (
-    <Stack>
+    <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="sign-up/SignUp" />
       <Stack.Screen name="sign-in/SignIn" />
     </Stack>
