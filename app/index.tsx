a2b52193d4e111a79ff8e9eb09bac2844d586eac
Bruno Tavares <bdmrusso@gmail.com>
2025-04-12 17:57:15 -0300

    feat: adds verification to check if user is already logged in

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,49 @@
+import { BackgroundGradient } from "@/domain/components/templates/background-gradient/BackgroundGradient";
+import { USER_DATA_KEY } from "@/domain/constants/async-storage-user";
+import { setUserDataFromAsyncStorage } from "@/redux/features/auth/authSlice";
+import { useAppDispatch } from "@/redux/hooks";
+import { getItemAsyncStorage } from "@/utils/AsyncStorage";
 import { Redirect } from "expo-router";
+import { useEffect, useState } from "react";
+import { ActivityIndicator, View } from "react-native";
 
 export default function Entry() {
+  const dispatch = useAppDispatch()
+  const [loadingStates, setLoadingState] = useState(true)
+  const [hasError, setHasError] = useState(false)
+
+  async function getUserData() {
+    try{
+      const result = await getItemAsyncStorage(USER_DATA_KEY)
+
+      if (!result) {
+        setLoadingState(false)
+        setHasError(true)
+        return
+      }
+
+      dispatch(setUserDataFromAsyncStorage(result))
+      setLoadingState(false)
+    } catch(error) {
+      setHasError(true)
+    } finally {
+      setLoadingState(false)
+    }
+  }
+
+  useEffect(() => {
+    getUserData()
+  }, [])
+
+  if (loadingStates) {
+    return (
+      <BackgroundGradient>
+        <View>
+          <ActivityIndicator />
+        </View>
+      </BackgroundGradient>
+    )
+  }
+
   return <Redirect href="/(unauthenticated)/sign-in/SignIn" />;
 }
