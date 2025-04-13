91fc8d7f6bec222e1984c75b959c4e09de189c81
Bruno Tavares <bdmrusso@gmail.com>
2025-04-13 11:51:27 -0300

    refactor: changes the style's location

--- app/(unauthenticated)/sign-in/SignIn.tsx
+++ app/(unauthenticated)/sign-in/SignIn.tsx
@@ -1,4 +1,3 @@
-import { colors } from "@/theme/colors";
 import { useState } from "react";
 import { View, Text, Pressable, ActivityIndicator } from "react-native";
 import Entypo from "@expo/vector-icons/Entypo";
@@ -10,7 +9,7 @@ import { BackgroundGradient } from "@/domain/components/templates/background-gra
 import { TextField } from "@/domain/components/atoms/text-field/TextField";
 import { ButtonAction } from "@/domain/components/atoms/button-action/ButtonAction";
 
-import { styles } from "./SigIn.styles"; // Importando os estilos
+import { styles } from "../../../domain/styles/SigIn.styles"; // Importando os estilos
 
 export default function SignInScreen() {
   const [isVisible, setIsVisible] = useState(false)
@@ -36,12 +35,12 @@ export default function SignInScreen() {
         Swift {' '}
         <Text style={{ fontWeight: 300 }}>
           Bank
-          </Text>
+        </Text>
       </Text>
       <Text
-       style={styles.subtitle}>Login</Text>
+        style={styles.subtitle}>Login</Text>
       <Text style={styles.welcomeText}>Boas-Vindas</Text>
-      
+
       <View style={{ marginBottom: 16 }}>
         <Text style={styles.label}>Insira seu email</Text>
         <TextField
