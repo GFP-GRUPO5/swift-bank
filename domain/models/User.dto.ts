c5fa2170a4ce339f7088684e412a5986d0e13805
Bruno Tavares <bdmrusso@gmail.com>
2025-04-13 11:47:53 -0300

    chore: adds js docs

--- domain/models/User.dto.ts
+++ domain/models/User.dto.ts
@@ -1,16 +1,21 @@
 export interface UserDTO {
-  id: string | number
+  id: string
   name: string
   lastName: string
   email: string
-  createdAt: Date
-  updatedAt: Date
+  createdAt: string
+  updatedAt: string
 }
 
+/**
+ * @field id: string
+ * @field name: string
+ * @field lastName: string
+ * @field email: string
+ */
 export interface CreateUserDOT {
+  id: string
   name: string
   lastName: string
   email: string
 }
-
-export interface FEUserDOT extends Partial<UserDTO> { }
