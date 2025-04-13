440ffa10955fd6789e0b2f5f04b705ac1cec219b
Bruno Tavares <bdmrusso@gmail.com>
2025-04-13 11:45:59 -0300

    fix: changes thunk id to correct flow, auth

--- redux/features/auth/thunks/sign-in.ts
+++ redux/features/auth/thunks/sign-in.ts
@@ -1,12 +1,11 @@
 import { AuthService } from "@/services/auth.service";
 import { SignInUserDTO } from "@/domain/types/auth.types";
 import { createAsyncThunk, isRejectedWithValue } from "@reduxjs/toolkit";
-import AsyncStorage from "@react-native-async-storage/async-storage";
 import { USER_DATA_KEY } from "@/domain/constants/async-storage-user";
 import { setItemAsyncStorage } from "@/utils/AsyncStorage";
 
 export const signInUserWithEmail = createAsyncThunk(
-  'user/signInUserWithEmail',
+  'auth/signInUserWithEmail',
   async (data: SignInUserDTO) => {
     try {
       const result = await AuthService.signIn(data)
