be50931faa4947abfdf5fa724531f4dd8a75a461
Rodrigo Harder <rodrigo.harder@alura.com.br>
2025-04-14 14:01:38 -0300

    refactor: add navigation to the code verification page

--- app/(unauthenticated)/forgot-password/ForgotPassword.tsx
+++ app/(unauthenticated)/forgot-password/ForgotPassword.tsx
@@ -1,13 +1,25 @@
 import { SwiftBankLogo } from "@/domain/components/icons/swiftBankLogo";
 import { BackgroundGradient } from "@/domain/components/templates/background-gradient/BackgroundGradient";
-import { Text, TouchableOpacity, View } from "react-native";
+import { Alert, Text, TouchableOpacity, View } from "react-native";
 import { forgotPasswordStyles } from "./ForgotPassword.styles";
 import { TextField } from "@/domain/components/atoms/text-field/TextField";
 import { ButtonAction } from "@/domain/components/atoms/button-action/ButtonAction";
-import { useNavigation } from "expo-router";
+import { router } from "expo-router";
+import { useState } from "react";
 
 export default function ForgotPassword() {
-  const navigation = useNavigation()
+  const [email, setEmail] = useState("");
+
+  const handleSend = () => {
+    if (!email.trim()) {
+      Alert.alert("Erro", "Por favor, insira um e-mail válido.");
+      return;
+    }
+    router.push({
+      pathname: "../verification-code/VerificationCode",
+      params: { email }
+    });
+  };
   return (
     <BackgroundGradient style={{ flex: 1 }}>
       <View style={forgotPasswordStyles.logo}>
@@ -20,13 +32,15 @@ export default function ForgotPassword() {
         <TextField
           placeholder="E-mail"
           autoCapitalize="none"
+          value={email}
+          onChangeText={setEmail}
         />
       </View>
-      <ButtonAction style={forgotPasswordStyles.button}>
+      <ButtonAction style={forgotPasswordStyles.button} onPress={handleSend}>
         <Text style={forgotPasswordStyles.buttonText}>Enviar</Text>
       </ButtonAction>
       <Text style={forgotPasswordStyles.backToSignInText}>Lembrou sua senha?</Text>
-      <TouchableOpacity onPress={() => navigation.goBack()}>
+      <TouchableOpacity onPress={() => router.back()}>
         <Text style={forgotPasswordStyles.backToSignInLink}>Faça seu Login!</Text>
       </TouchableOpacity>
     </BackgroundGradient>
