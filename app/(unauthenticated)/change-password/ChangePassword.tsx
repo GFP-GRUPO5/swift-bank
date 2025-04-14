d8f9dcac483034d48a8aa7bbb75ad8a47a07cae6
Rodrigo Harder <rodrigo.harder@alura.com.br>
2025-04-14 15:05:36 -0300

    feat: add change password page

---
+++ app/(unauthenticated)/change-password/ChangePassword.tsx
@@ -0,0 +1,96 @@
+import { SwiftBankLogo } from "@/domain/components/icons/swiftBankLogo";
+import { BackgroundGradient } from "@/domain/components/templates/background-gradient/BackgroundGradient";
+import { Alert, Text, View, Pressable } from "react-native";
+import { TextField } from "@/domain/components/atoms/text-field/TextField";
+import { ButtonAction } from "@/domain/components/atoms/button-action/ButtonAction";
+import { changePasswordStyles } from "./ChangePassword.styles";
+import { useRouter } from "expo-router";
+import { useState } from "react";
+import Entypo from "@expo/vector-icons/Entypo";
+
+export default function VerificationCode() {
+  const router = useRouter();
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleChangePassword = () => {
+    if (password === "" || confirmPassword === "") {
+      Alert.alert("Por favor, preencha todos os campos!");
+      return;
+    }
+
+    if (password !== confirmPassword) {
+      Alert.alert("As senhas não coincidem!");
+      return;
+    }
+
+    Alert.alert("Sucesso", "Senha alterada com sucesso!", [
+      {
+        text: "OK",
+        onPress: () => router.replace("../sign-in/SignIn"),
+      },
+    ]);
+  };
+
+  return (
+    <BackgroundGradient style={{ flex: 1 }}>
+      <View style={changePasswordStyles.logo}>
+        <SwiftBankLogo />
+      </View>
+      <Text style={changePasswordStyles.title}>Altere a sua Senha</Text>
+      <Text style={changePasswordStyles.subtitle}>Digite sua nova senha.</Text>
+
+      <View style={{ marginBottom: 16 }}>
+        <Text style={changePasswordStyles.label}>Senha</Text>
+        <View style={changePasswordStyles.passwordContainer}>
+          <TextField
+            placeholder="Insira a senha"
+            autoCapitalize="none"
+            secureTextEntry={!isVisible}
+            value={password}
+            onChangeText={setPassword}
+            style={{ paddingRight: 56 }}
+          />
+          <Pressable
+            style={changePasswordStyles.eyeIcon}
+            onPress={() => setIsVisible((prev) => !prev)}
+          >
+            {isVisible ? (
+              <Entypo name="eye-with-line" size={24} color="black" />
+            ) : (
+              <Entypo name="eye" size={24} color="black" />
+            )}
+          </Pressable>
+        </View>
+      </View>
+
+      <View style={{ marginBottom: 16 }}>
+        <Text style={changePasswordStyles.label}>Confirmação de senha</Text>
+        <View style={changePasswordStyles.passwordContainer}>
+          <TextField
+            placeholder="Confirme sua senha"
+            autoCapitalize="none"
+            secureTextEntry={!isVisible}
+            value={confirmPassword}
+            onChangeText={setConfirmPassword}
+            style={{ paddingRight: 56 }}
+          />
+          <Pressable
+            style={changePasswordStyles.eyeIcon}
+            onPress={() => setIsVisible((prev) => !prev)}
+          >
+            {isVisible ? (
+              <Entypo name="eye-with-line" size={24} color="black" />
+            ) : (
+              <Entypo name="eye" size={24} color="black" />
+            )}
+          </Pressable>
+        </View>
+      </View>
+      <ButtonAction style={changePasswordStyles.button} onPress={handleChangePassword}>
+        <Text style={changePasswordStyles.buttonText}>Alterar</Text>
+      </ButtonAction>
+    </BackgroundGradient>
+  );
+}
